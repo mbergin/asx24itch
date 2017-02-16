327935811f8388274993ec57626e206bd918fa4e
Matt Bergin <mattbergin0@gmail.com>
2017-02-16 17:55:17 +0000

    Use spaces for indentation

--- example.cpp
+++ example.cpp
@@ -7,11 +7,11 @@ int main()
 {
     // An "Order Added" message
     const char msg[] = {
-        'A', 		                                                    // messageType: OrderAdded
+        'A',                                                             // messageType: OrderAdded
         '\xFF', '\xEE', '\xDD', '\xCC',                                 // timestamp: 4293844428
         '\xFF', '\xEE',                                                 // tradeDate: 65518
         '\xFF', '\xEE', '\xDD', '\xCC',                                 // contractNumber: 4293844428
-        'B',		                                                    // side: Buy
+        'B',                                                            // side: Buy
         '\xFF', '\xEE', '\xDD', '\xCC', '\xBB', '\xAA', '\x99', '\x88', // orderNumber: 18441921395520346504
         '\xFF', '\xEE', '\xDD', '\xCC',                                 // orderBookPriority: 4293844428
         '\xFF', '\xEE', '\xDD', '\xCC',                                 // quantity: 4293844428
